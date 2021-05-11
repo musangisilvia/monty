5b01433261a2aa5a4e8121346366afae579a0cb6
musangisilvia <musangisilvia@gmail.com>
2021-05-11 20:40:07 +0000

    add a function that does nothing

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  * nop - doesn't do anything.
+  * @stack: double pointer to stack.
+  * @line_number: number of lines.
+  *
+  * Return: void.
+  */
+void nop(stack_t **stack, __attribute__((unused))unsigned int line_number)
+{
+	(void)stack;
+}
