a2f52c1486bcdcc0103e30b12fe00905da6c981d
musangisilvia <musangisilvia@gmail.com>
2021-05-11 19:15:27 +0000

    add get_opcode.c file

---
+++ get_opcode.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+  * get_opcode - checks whether the opcode exists.
+  * @token: opcode passed.
+  *
+  * Return: pointer to function for opcode or NULL.
+  */
+void (*get_opcode(char *token))(stack_t **head, unsigned int line_number)
+{
+	instruction_t opc[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (opc[i].opcode)
+	{
+		if (strcmp(opc[i].opcode, token) == 0)
+			return (opc[i].f);
+		i++;
+	}
+	return (NULL);
+}
