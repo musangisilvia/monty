99b94a71b59e2ffea140cc5e45ea559a44d3e5eb
musangisilvia <musangisilvia@gmail.com>
2021-05-12 12:38:47 +0000

    memory leak

--- main.c
+++ main.c
@@ -17,16 +17,10 @@ int main(int argc, char **argv)
 	void (*op_func)(stack_t **stack, unsigned int line_number);
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+		err(1);
 	fptr = fopen(argv[1], "r");
 	if (fptr == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+		err(2, argv[1]);
 	while (getline(&line, &size, fptr) != -1)
 	{
 		if (!strcmp(line, "\n"))
@@ -37,30 +31,18 @@ int main(int argc, char **argv)
 		tokens = break_line(line), op_func = get_opcode(tokens[0]);
 		if (op_func == NULL)
 		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_count, tokens[0]);
-			exit(EXIT_FAILURE);
+			free(line), free(tokens), fclose(fptr), free_dlist(stack);
+			err(3, line_count, tokens[0]);
 		}
 		if (strcmp(tokens[0], "push") == 0 && tokens[1])
 			argument = tokens[1];
-		else if (!strcmp(tokens[0], "push") && !tokens[1])
-			err(&stack, line_count);
+		else if (!strcmp(tokens[0], "push") && tokens[1] == NULL)
+		{
+			free(line), free(tokens), fclose(fptr), free_dlist(stack);
+			err(5, line_count);
+		}
 		op_func(&stack, line_count), line_count++, free(tokens);
 	}
-	fclose(fptr);
-	free(line), free_dlist(stack);
+	fclose(fptr), free(line), free_dlist(stack);
 	return (0);
-}
-
-/**
-  * err - print integer fault.
-  * @stack: double pointer to stack;
-  * @line_number: number of lines.
-  *
-  * Return: void.
-  */
-void err(stack_t **stack, unsigned int line_number)
-{
-	free_dlist(*stack);
-	fprintf(stderr, "L%d: usage: push integer\n", line_number);
-	exit(EXIT_FAILURE);
 }
