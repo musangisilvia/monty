2dd61190247c704489af6fe0d23ca545e673269c
musangisilvia <musangisilvia@gmail.com>
2021-05-12 06:04:51 +0000

    correct error statement

--- push.c
+++ push.c
@@ -105,7 +105,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	sum = (*stack)->n + (*stack)->next->n;
